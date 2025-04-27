61b9c15991c4b2231c8ed0bfb3886c5ec6989797
Marcin Mosiejko <marcin@stratokit.io>
2025-04-28 01:06:41 +0200

    feat: title

--- apps/frontend/src/components/pages/home.tsx
+++ apps/frontend/src/components/pages/home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <PageWrap>
       <div className="flex flex-1 flex-col items-center justify-center space-y-4">
-        <h1 className="text-primary text-4xl font-bold">Reports App</h1>
+        <h1 className="text-primary text-4xl font-bold">Reports Gen</h1>
         <Link to="/reports">
           <Button>{`Go to Reports >`}</Button>
         </Link>
